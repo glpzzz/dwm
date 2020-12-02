0188c8d8205f21cc8347444a25d21398525bfa99
Gabriel A. López López <glpz@daxslab.com>
2020-12-02 00:16:03 -0500

    clean warnings

--- dwm.c
+++ dwm.c
@@ -811,8 +811,6 @@ void
 drawbar(Monitor *m)
 {
   int x, w, tw = 0, n = 0, scm;
-	int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
