9b92e6ed13e8590ca19537994f13a3ef764800d7
Gabriel A. López López <glpz@daxslab.com>
2022-11-25 17:25:47 -0500

    windows rules

--- config.h
+++ config.h
@@ -34,18 +34,23 @@ static const Rule rules[] = {
          *	WM_CLASS(STRING) = instance, class
          *	WM_NAME(STRING) = title
          */
-        /* class      instance    title tags mask           isfloating   monitor */
-        {"Gimp",            NULL, NULL,                     0,      0, -1},
-        {"Firefox",         NULL, NULL,                     0,      0, -1},
-        {"ssh-askpass",     NULL, NULL,                     0,      1, -1},
-        {"XCalc",           NULL, NULL,                     0,      1, -1},
-        {"jetbrains-clion", NULL, "splash",                 0,      1, -1},
-        {NULL,              NULL, "zoom ",                  1<<7,   1, -1},
-        {NULL,              NULL, "Zoom Meeting",           1<<7,   0, -1},
-        {NULL,              NULL, "Zoom Cloud Meetings",    1<<7,   0, -1},
-        {"Thunderbird",     NULL, NULL,                     1<<6,   0, -1},
-        {"TelegramDesktop", NULL, NULL,                     2,      0, -1},
-        {"Skype",           NULL, NULL,                     2,      0, -1},
+        /* class                instance    title                   tags-mask   isfloating  monitor */
+        {"Gimp",                NULL,       NULL,                   0,          0,          -1},
+        {"Firefox",             NULL,       NULL,                   0,          0,          -1},
+        {"ssh-askpass",         NULL,       NULL,                   0,          1,          -1},
+        {"XCalc",               NULL,       NULL,                   0,          1,          -1},
+        {"jetbrains-clion",     NULL,       "splash",               0,          1,          -1},
+        {"jetbrains-idea",      NULL,       "splash",               0,          1,          -1},
+        {"jetbrains-phpstorm",  NULL,       "splash",               0,          1,          -1},
+        {"jetbrains-webstorm",  NULL,       "splash",               0,          1,          -1},
+        {"jetbrains-pycharm",   NULL,       "splash",               0,          1,          -1},
+        {"zoom",                NULL,       "Settings",             1<<7,       0,          -1},
+        {"zoom",                NULL,       "Zoom Meeting",         1<<7,       0,          -1},
+        {"zoom",                NULL,       "Zoom - Free Account",  1<<7,       0,          -1},
+        {"zoom",                NULL,       "",                     1<<7,       1,          -1},
+        {"Thunderbird",         NULL,       NULL,                   1<<6,       0,          -1},
+        {"TelegramDesktop",     NULL,       NULL,                   2,          0,          -1},
+        {"Skype",               NULL,       NULL,                   2,          0,          -1},
 };
 
 /* layout(s) */
