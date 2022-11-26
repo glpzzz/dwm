b4155be4c7d3bbe600f77e393e8a66c855c3831e
Gabriel A. López López <glpz@daxslab.com>
2022-11-26 02:06:47 -0500

    add clients rules

--- config.def.h
+++ config.def.h
@@ -26,13 +26,27 @@ static char *colors[][3] = {
 static char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+        /* xprop(1):
+         *	WM_CLASS(STRING) = instance, class
+         *	WM_NAME(STRING) = title
+         */
+        /* class                instance    title                   tags-mask   isfloating  monitor */
+        {"Gimp",                NULL,       NULL,                   0,          0,          -1},
+        {"Firefox",             NULL,       NULL,                   0,          0,          -1},
+        {"ssh-askpass",         NULL,       NULL,                   0,          1,          -1},
+        {"XCalc",               NULL,       NULL,                   0,          1,          -1},
+        {"jetbrains-clion",     NULL,       "splash",               0,          1,          -1},
+        {"jetbrains-idea",      NULL,       "splash",               0,          1,          -1},
+        {"jetbrains-phpstorm",  NULL,       "splash",               0,          1,          -1},
+        {"jetbrains-webstorm",  NULL,       "splash",               0,          1,          -1},
+        {"jetbrains-pycharm",   NULL,       "splash",               0,          1,          -1},
+        {"zoom",                NULL,       "Settings",             1<<7,       0,          -1},
+        {"zoom",                NULL,       "Zoom Meeting",         1<<7,       0,          -1},
+        {"zoom",                NULL,       "Zoom - Free Account",  1<<7,       0,          -1},
+        {"zoom",                NULL,       "",                     1<<7,       1,          -1},
+        {"Thunderbird",         NULL,       NULL,                   1<<6,       0,          -1},
+        {"TelegramDesktop",     NULL,       NULL,                   2,          0,          -1},
+        {"Skype",               NULL,       NULL,                   2,          0,          -1},
 };
 
 /* layout(s) */
