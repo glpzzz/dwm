1ac1070cec1e5804d7be8f17e575f31f73938fcb
Gabriel A. López López <glpz@daxslab.com>
2023-10-05 10:48:13 -0400

    adjust commands in config.def.h

--- config.def.h
+++ config.def.h
@@ -81,8 +81,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 
 /*
  * Xresources preferences to load at startup
